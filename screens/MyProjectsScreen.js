5be646012598a6f77ec41eae9bd16122e545d4e9
Rommel Rico <me@rommelrico.com>
2020-01-05 20:38:06 -0800

    :tada: Screens: MyProjectsScreen

---
+++ screens/MyProjectsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MyProjectsScreen = (props) => {
+  return (
+    <View>
+      <Text>My Projects</Text>
+    </View>
+  );
+};
+
+export default MyProjectsScreen;
