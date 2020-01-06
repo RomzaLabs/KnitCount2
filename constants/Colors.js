3d6cf2e465a7c3fc3e13cf5c7d67926c4af7d9a1
Rommel Rico <me@rommelrico.com>
2020-01-06 06:58:38 -0800

    :tada: Constants: Colors

---
+++ constants/Colors.js
@@ -0,0 +1,8 @@
+export default {
+  primaryColor: "#1E90FF",
+  clearChillColor: "#1E90FF",
+  watermelonColor: "#FF4757",
+  brightGreekColor: "#3742FA",
+  coralColor: "#FF7F50",
+  ufoColor: "#2ED573"
+};
