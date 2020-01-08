86a6fa860295ddf69c43957a36124ec1bd8187f5
Rommel Rico <me@rommelrico.com>
2020-01-08 07:07:09 -0800

    :tada: Screens: ProjectDetailsScreen

---
+++ screens/ProjectDetailsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ProjectDetailsScreen = (props) => {
+  return (
+    <View>
+      <Text>Project Details</Text>
+    </View>
+  );
+};
+
+export default ProjectDetailsScreen;
