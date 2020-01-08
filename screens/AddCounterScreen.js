723584fb0a718b7a481f346007d3d96b2387806d
Rommel Rico <me@rommelrico.com>
2020-01-08 07:06:47 -0800

    :tada: Screens: AddCounterScreen

---
+++ screens/AddCounterScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AddCounterScreen = (props) => {
+  return (
+    <View>
+      <Text>Add Counter</Text>
+    </View>
+  );
+};
+
+export default AddCounterScreen;
