8dd734050c152874a7ab23bd40f376ededc3b865
Rommel Rico <me@rommelrico.com>
2020-01-08 07:06:58 -0800

    :tada: Screens: CreateProjectScreen

---
+++ screens/CreateProjectScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CreateProjectScreen = (props) => {
+  return (
+    <View>
+      <Text>Create Project</Text>
+    </View>
+  );
+};
+
+export default CreateProjectScreen;
