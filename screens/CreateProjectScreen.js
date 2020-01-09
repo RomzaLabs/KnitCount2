38af90dcdf8c3fc7e48be07fc953fec4454ef56a
Rommel Rico <me@rommelrico.com>
2020-01-08 18:28:53 -0800

    :sparkles: Navigation: Setting up Navigation in CreateProjectScreen

--- screens/CreateProjectScreen.js
+++ screens/CreateProjectScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+
+import Colors from "../constants/Colors";
 
 const CreateProjectScreen = (props) => {
   return (
     <View>
       <Text>Create Project</Text>
+      <Button
+        color={Colors.primaryColor}
+        title="Enter Name"
+        onPress={() => props.navigation.navigate("ProjectDetails")}
+      />
     </View>
   );
 };
