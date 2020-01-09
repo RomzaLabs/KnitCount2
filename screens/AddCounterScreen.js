8212a559c71748bcc02790f1f9ea29872d92f1b5
Rommel Rico <me@rommelrico.com>
2020-01-08 18:33:29 -0800

    :sparkles: Navigation: Setting up Navigation on AddCounterScreen

--- screens/AddCounterScreen.js
+++ screens/AddCounterScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text, Button} from 'react-native';
+
+import Colors from "../constants/Colors";
 
 const AddCounterScreen = (props) => {
   return (
     <View>
       <Text>Add Counter</Text>
+      <Button
+        color={Colors.primaryColor}
+        title="Preset"
+        onPress={() => props.navigation.navigate("ProjectDetails")}
+      />
     </View>
   );
 };
