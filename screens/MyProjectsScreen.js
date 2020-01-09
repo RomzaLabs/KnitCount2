0a84a3cd7c75bc691f2f6c41820eb8ac4bf2ecbe
Rommel Rico <me@rommelrico.com>
2020-01-08 18:27:12 -0800

    :sparkles: Navigation: Setting up Navigation in MyProjectsScreen

--- screens/MyProjectsScreen.js
+++ screens/MyProjectsScreen.js
@@ -1,14 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform, Button } from 'react-native';
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import KnitCountHeaderButton from "../components/KnitCountHeaderButton";
+import Colors from "../constants/Colors";
 
 const MyProjectsScreen = (props) => {
   return (
     <View>
       <Text>My Projects</Text>
+      <Button
+        color={Colors.primaryColor}
+        title="Add Project"
+        onPress={() => props.navigation.navigate("CreateProject")}
+      />
+      <Button
+        color={Colors.primaryColor}
+        title="Project Details"
+        onPress={() => props.navigation.navigate("ProjectDetails")}
+      />
     </View>
+  );
+};
+
+MyProjectsScreen.navigationOptions = (navData) => {
+  return (
+    {
+      headerTitle: "My Projects",
+      headerLeft: () => {
+        return (
+          <HeaderButtons HeaderButtonComponent={KnitCountHeaderButton}>
+            <Item
+              title="Settings"
+              iconName={Platform.OS === "android" ? "md-settings" : "ios-settings"}
+              onPress={() => navData.navigation.navigate('Settings')}
+            />
+          </HeaderButtons>
+        );
+      }
+    }
   );
 };
 
