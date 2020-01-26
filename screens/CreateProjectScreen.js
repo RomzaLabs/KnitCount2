f31e7a7837efb3fcb4af442b6eb74c3c3d4e701a
Rommel Rico <me@rommelrico.com>
2020-01-26 11:08:17 -0800

    :sparkles: AddProject: Adding KeyboardAvoidingView to screen

--- screens/CreateProjectScreen.js
+++ screens/CreateProjectScreen.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { Button, KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native';
 
 import Colors from "../constants/Colors";
 
 const CreateProjectScreen = (props) => {
   return (
-    <View>
-      <Text>Create Project</Text>
-      <Button
-        color={Colors.primaryColor}
-        title="Enter Name"
-        onPress={() => props.navigation.navigate("ProjectDetails")}
-      />
-    </View>
+    <KeyboardAvoidingView behavior='padding' keyboardVerticalOffset={50} style={styles.screen}>
+      <View>
+        <Text>Create Project</Text>
+        <Button
+          color={Colors.primaryColor}
+          title="Enter Name"
+          onPress={() => props.navigation.navigate("ProjectDetails")}
+        />
+      </View>
+    </KeyboardAvoidingView>
   );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1
+  }
+});
 
 export default CreateProjectScreen;
