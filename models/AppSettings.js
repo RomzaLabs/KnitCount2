e2a9c9bf45f214eacde6aaefe8bceaf9794ed170
Rommel Rico <me@rommelrico.com>
2020-01-26 11:21:59 -0800

    :sparkles: AddProject: Adding main text color to AppSettings model

--- models/AppSettings.js
+++ models/AppSettings.js
@@ -1,9 +1,10 @@
 import Colors from "../constants/Colors";
 
 class AppSettings {
-  constructor(isPremium = false, mainColor = Colors.primaryColor) {
+  constructor(isPremium = false, mainColor = Colors.primaryColor, mainTextColor = Colors.primaryTextColor) {
     this.isPremium = isPremium;
     this.mainColor = mainColor;
+    this.mainTextColor = mainTextColor;
   }
 }
 
