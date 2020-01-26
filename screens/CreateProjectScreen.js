5e3f602bcfa719f1c1380717ce22b83a38fc97b1
Rommel Rico <me@rommelrico.com>
2020-01-26 12:52:29 -0800

    :sparkles: AddProject: Adding label for project name

--- screens/CreateProjectScreen.js
+++ screens/CreateProjectScreen.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, KeyboardAvoidingView, Platform, StyleSheet, Text, View} from 'react-native';
 import { observer } from "mobx-react";
 
 import AppSettingsStore from "../store/AppSettingsStore";
+import Project from "../models/Project";
 
 const CreateProjectScreen = (props) => {
+  const [project, setProject] = useState(null);
+
+  if (!project) setProject(new Project(0, "My new project"));
+
+  const renderProjectName = () => project ? project.name : "";
+
   return (
     <KeyboardAvoidingView
       behavior='padding'
       keyboardVerticalOffset={50}
       style={[styles.screen, {backgroundColor: AppSettingsStore.mainColor}]}
     >
-      <View>
-        <Text>Create Project</Text>
+      <View style={styles.projectContainer}>
+        <Text style={[styles.projectName, {color: AppSettingsStore.mainTextColor}]}>
+          {renderProjectName()}
+        </Text>
         <Button
           color={AppSettingsStore.mainTextColor}
           title="Enter Name"
@@ -37,6 +46,19 @@ CreateProjectScreen.navigationOptions = (navData) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1
+  },
+  projectContainer: {
+    height: '100%',
+    maxHeight: '40%',
+    margin: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  projectName: {
+    fontSize: 32,
+    fontFamily: "avenir-roman",
+    width: "100%",
+    textAlign: "center"
   }
 });
 
