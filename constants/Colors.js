d2d2becd9314926450b4700dbb7bae63b4511346
Rommel Rico <me@rommelrico.com>
2020-01-26 10:50:25 -0800

    :lipstick: AddProject: Adding new text colors

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,8 +1,14 @@
 export default {
   primaryColor: "#1E90FF",
+  primaryTextColor: "#FDFDFD",
   clearChillColor: "#1E90FF",
+  clearChillTextColor: "#FDFDFD",
   watermelonColor: "#FF4757",
+  watermelonTextColor: "#FDFDFD", // TODO
   brightGreekColor: "#3742FA",
+  brightGreekTextColor: "#FDFDFD", // TODO
   coralColor: "#FF7F50",
-  ufoColor: "#2ED573"
+  coralTextColor: "#FDFDFD", // TODO
+  ufoColor: "#2ED573",
+  ufoTextColor: "#FDFDFD", // TODO
 };
