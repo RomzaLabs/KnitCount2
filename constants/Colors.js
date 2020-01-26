94dbf8cf7587a431cac08426a149b4561c8479e4
Rommel Rico <me@rommelrico.com>
2020-01-26 13:41:34 -0800

    :sparkles: AddProject: New background colors

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,14 +1,20 @@
 export default {
   primaryColor: "#1E90FF",
   primaryTextColor: "#FDFDFD",
+  primaryBGColor: "#56ABFF",
   clearChillColor: "#1E90FF",
   clearChillTextColor: "#FDFDFD",
+  clearChillBGColor: "#56ABFF",
   watermelonColor: "#FF4757",
   watermelonTextColor: "#FDFDFD", // TODO
+  watermelonBGColor: "#56ABFF", // TODO
   brightGreekColor: "#3742FA",
   brightGreekTextColor: "#FDFDFD", // TODO
+  brightGreekBGColor: "#56ABFF", // TODO
   coralColor: "#FF7F50",
   coralTextColor: "#FDFDFD", // TODO
+  coralBGColor: "#56ABFF", // TODO
   ufoColor: "#2ED573",
   ufoTextColor: "#FDFDFD", // TODO
+  ufoBGColor: "#56ABFF" // TODO
 };
