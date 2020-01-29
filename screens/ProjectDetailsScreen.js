9eda1536e2a3f6181bf6e31c03d9a610a484a4e3
Rommel Rico <me@rommelrico.com>
2020-01-28 21:26:19 -0800

    :sparkles: Gestures: Enabling back gesture again on ProjectDetailsScreen

--- screens/ProjectDetailsScreen.js
+++ screens/ProjectDetailsScreen.js
@@ -40,8 +40,7 @@ ProjectDetailsScreen.navigationOptions = (navData) => {
       headerStyle: { ...navData.navigationOptions.headerStyle, backgroundColor: AppSettingsStore.mainColor },
       headerTitleStyle: { ...navData.navigationOptions.headerTitleStyle, color: AppSettingsStore.mainTextColor },
       headerBackTitleStyle: { ...navData.navigationOptions.headerBackTitleStyle, color: AppSettingsStore.mainTextColor },
-      headerTintColor: AppSettingsStore.mainTextColor,
-      gestureEnabled: false
+      headerTintColor: AppSettingsStore.mainTextColor
     }
   );
 };
