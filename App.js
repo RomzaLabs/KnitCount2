e687ac1e861d78b52868ce0f058aed84bbfe2b1f
Rommel Rico <me@rommelrico.com>
2020-01-29 21:24:25 -0800

    :sparkles: Persistence: Handling loading user settings

--- App.js
+++ App.js
@@ -9,7 +9,15 @@ import AppSettingsStore from "./store/AppSettingsStore";
 import AppSettings from "./models/AppSettings";
 import {FilterPreference} from "./models/FilterPreference";
 import Colors from "./constants/Colors";
-import { initProjects, initSettings, fetchProjects, fetchCountersForProject, fetchImageUrisForProject } from './store/db';
+import {
+  initProjects,
+  initSettings,
+  fetchSettings,
+  insertSettings,
+  fetchProjects,
+  fetchCountersForProject,
+  fetchImageUrisForProject
+} from './store/db';
 import Counter from "./models/Counter";
 import Project from "./models/Project";
 
@@ -26,16 +34,31 @@ const loadFonts = async () => {
 };
 
 const loadSettings = async() => {
-  // Temporary handling.
-  const appSettings = new AppSettings(
-    true,
-    Colors.primaryColor,
-    Colors.primaryTextColor,
-    Colors.primaryBGColor,
-    FilterPreference.ALL
-  );
-  AppSettingsStore.setSettings(appSettings);
-  // End temporary handling.
+  const dbResult = await fetchSettings();
+  console.log("dbResult: ", dbResult);
+  const isBrandNewUser = dbResult.rows.length === 0;
+  if (isBrandNewUser) {
+    const defaultSettings = new AppSettings(
+      false,
+      Colors.primaryColor,
+      Colors.primaryTextColor,
+      Colors.primaryBGColor,
+      FilterPreference.WIP
+    );
+    AppSettingsStore.setSettings(defaultSettings);
+    await insertSettings(defaultSettings);
+  } else {
+    const dbSettings = dbResult.rows._array[0];
+    const userSettings = new AppSettings(
+      dbSettings.is_premium,
+      dbSettings.main_color,
+      dbSettings.main_text_color,
+      dbSettings.main_bg_color,
+      dbSettings.filter_preference
+    );
+    console.log("userSettings: ", userSettings);
+    AppSettingsStore.setSettings(userSettings);
+  }
 };
 
 const loadProjects = async() => {
@@ -48,11 +71,11 @@ const loadProjects = async() => {
 
     const dbCountersResult = await fetchCountersForProject(projectId);
     const dbCounters = dbCountersResult.rows._array;
-    const counters = dbCounters.map(c => new Counter(c.id, c.project_id, c.label, c.value, c.stepsPerCount));
+    const counters = dbCounters.map(c => new Counter(c.id, c.project_id, c.label, c.value, c.steps_per_count));
 
     const dbImageUrisResult = await fetchImageUrisForProject(projectId);
     const dbImageUris = dbImageUrisResult.rows._array;
-    const imageUris = dbImageUris.map(i => i.imageUri);
+    const imageUris = dbImageUris.map(i => i.image_uri);
 
     const project = new Project(
       projectId,
@@ -61,9 +84,9 @@ const loadProjects = async() => {
       counters,
       dbProject.notes,
       imageUris,
-      dbProject.startDate,
-      dbProject.modifiedDate,
-      dbProject.endDate
+      dbProject.start_date,
+      dbProject.modified_date,
+      dbProject.end_date
     );
     projects.push(project);
   }
@@ -78,7 +101,7 @@ const loadKnitCount = async () => {
   }
 
   try {
-    await Promise.resolve("settings");
+    await loadSettings();
   } catch (e) {
     console.error("Error loading settings.", e);
   }
