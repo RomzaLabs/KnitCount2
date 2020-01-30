3b5561d48857b91f13b559bd450fccf1095a9035
Rommel Rico <me@rommelrico.com>
2020-01-29 22:04:30 -0800

    :sparkles: Persistence: Image model

---
+++ models/Image.js
@@ -0,0 +1,10 @@
+class Image {
+  constructor(id, projectId, imageUri, dateAdded) {
+    this.id = id;
+    this.projectId = projectId;
+    this.imageUri = imageUri;
+    this.dateAdded = dateAdded;
+  }
+}
+
+export default Image;
