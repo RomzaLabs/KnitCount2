115095fef6c0fc8ed8d3f9933809ffd6973ffc5e
Rommel Rico <me@rommelrico.com>
2020-02-01 16:36:18 -0800

    :sparkles: ProjectDetails: Setting up SectionList using Section details

--- screens/ProjectDetailsScreen.js
+++ screens/ProjectDetailsScreen.js
@@ -7,14 +7,22 @@ import ProjectsStore from "../store/ProjectsStore";
 import KnitCountHeaderButton from "../components/KnitCountHeaderButton";
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 
+import
+  SECTION_DETAILS,
+  {
+    MARK_FINISHED_BTN_ID,
+    UPDATE_TITLE_BTN_ID,
+    DELETE_PROJECT_BTN_ID
+  } from "../constants/SECTION_DETAILS";
+
 const ProjectDetailsScreen = (props) => {
   const { selectedProject } = ProjectsStore;
 
-  const DATA = [
-    { title: "projects", data: [] },
-    { title: 'photos', data: [] },
-    { title: 'notes', data: [] },
-    { title: 'actions', data: ['markFinishedBtn', 'updateTitleBtn', 'deleteProjectBtn'] }
+  const PROJECT_DETAILS_SECTIONS = [
+    { key: SECTION_DETAILS.COUNTERS.key, title: SECTION_DETAILS.COUNTERS.title, data: SECTION_DETAILS.COUNTERS.data },
+    { key: SECTION_DETAILS.PHOTOS.key, title: SECTION_DETAILS.PHOTOS.title, data: SECTION_DETAILS.PHOTOS.data },
+    { key: SECTION_DETAILS.NOTES.key, title: SECTION_DETAILS.NOTES.title, data: SECTION_DETAILS.NOTES.data },
+    { key: SECTION_DETAILS.ACTIONS.key, title: SECTION_DETAILS.ACTIONS.title, data: SECTION_DETAILS.ACTIONS.data }
   ];
 
   const renderActionBtn = (btnName) => {
@@ -34,10 +42,10 @@ const ProjectDetailsScreen = (props) => {
     >
       <SectionList
         style={{backgroundColor: AppSettingsStore.mainColor}}
-        sections={DATA}
+        sections={PROJECT_DETAILS_SECTIONS}
         keyExtractor={(item, index) => item + index}
         renderItem={({ section, item }) => {
-          if (section.title === 'actions') return renderActionBtn(item);
+          if (section.key === SECTION_DETAILS.ACTIONS.key) return renderActionBtn(item);
           return null;
         }}
         renderSectionHeader={({ section: { title } }) => <Text style={styles.header}>{title}</Text>}
