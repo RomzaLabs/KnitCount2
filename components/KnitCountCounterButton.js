bc5b27c1f3a0d82f2540e902b73f3db5b8921eb3
Rommel Rico <me@rommelrico.com>
2020-02-09 18:50:06 -0800

    :sparkles: Counter: Animating the Counter Button

--- components/KnitCountCounterButton.js
+++ components/KnitCountCounterButton.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Animated, Dimensions, StyleSheet, Text, View } from 'react-native';
 import PropTypes from "prop-types";
 import { TapGestureHandler, PanGestureHandler, LongPressGestureHandler, State } from 'react-native-gesture-handler';
 
 const MAX_ZEROES = 5;
 
 const KnitCountCounterButton = (props) => {
+  const [bounceAnim, setBounceAnim] = useState(new Animated.Value(1));
+
+  useEffect(() => {
+    Animated.spring(bounceAnim, {
+      toValue: 1,
+      friction: 4,
+      useNativeDriver: true
+    }).start();
+  }, [bounceAnim]);
+
   const digitsForCount = (count) => count.toString().length;
   const leadingZeroes = (count) => "0".repeat(MAX_ZEROES - digitsForCount(count));
 
   const onSingleTapEvent = (e) => {
     const { state } = e.nativeEvent;
+    if (state === State.ACTIVE) {
+      setBounceAnim(new Animated.Value(0.9));
+    }
     if (state === State.END) {
       const newValue = props.counter.value + props.counter.stepsPerCount;
       const newCounter = {...props.counter, value: newValue};
@@ -21,6 +34,9 @@ const KnitCountCounterButton = (props) => {
   const onDragEvent = (e) => e.nativeEvent.translationX > 0 ? onDragRight(e) : onDragLeft(e);
   const onDragRight = (e) => {
     const { state } = e.nativeEvent;
+    if (state === State.ACTIVE) {
+      setBounceAnim(new Animated.Value(0.9));
+    }
     if (state === State.END) {
       const newValue = props.counter.value + props.counter.stepsPerCount;
       const newCounter = {...props.counter, value: newValue};
@@ -29,6 +45,9 @@ const KnitCountCounterButton = (props) => {
   };
   const onDragLeft = (e) => {
     const { state } = e.nativeEvent;
+    if (state === State.ACTIVE) {
+      setBounceAnim(new Animated.Value(0.9));
+    }
     if (state === State.END) {
       const newValue = props.counter.value - props.counter.stepsPerCount;
       const newValueOrZero = newValue < 0 ? 0 : newValue;
@@ -40,6 +59,7 @@ const KnitCountCounterButton = (props) => {
   const onLongPressEvent = (e) => {
     const { state } = e.nativeEvent;
     if (state === State.ACTIVE) {
+      setBounceAnim(new Animated.Value(0.9));
       props.onLongPress(props.counter.id);
     }
   };
@@ -48,12 +68,17 @@ const KnitCountCounterButton = (props) => {
     <LongPressGestureHandler minDurationMs={600} onHandlerStateChange={onLongPressEvent}>
       <PanGestureHandler activeOffsetX={[-10, 10]} onHandlerStateChange={onDragEvent}>
         <TapGestureHandler numberOfTaps={1} onHandlerStateChange={onSingleTapEvent}>
-          <View style={[styles.countButton, {borderColor: props.mainTextColor, backgroundColor: props.mainColor}]}>
+          <Animated.View style={
+            [
+              styles.countButton,
+              { transform: [{scale: bounceAnim}], borderColor: props.mainTextColor, backgroundColor: props.mainColor }
+            ]
+          }>
             <Text style={styles.countLabel}>
               <Text style={{color: props.mainBGColor}}>{leadingZeroes(props.counter.value)}</Text>
               <Text style={{color: props.mainTextColor}}>{props.counter.value}</Text>
             </Text>
-          </View>
+          </Animated.View>
         </TapGestureHandler>
       </PanGestureHandler>
     </LongPressGestureHandler>
