07059c24146d155d0895b11acea4bf3e5989a9d7
Rommel Rico <me@rommelrico.com>
2020-02-18 21:52:15 -0800

    :bug: Stepper: Deleting a project should delete its images and counters

--- store/ProjectsStore.js
+++ store/ProjectsStore.js
@@ -10,7 +10,7 @@ import {
   deleteImage,
   insertCounter,
   updateCounter,
-  deleteCounter
+  deleteCounter, deleteCountersForProject, deleteImagesForProject
 } from "../store/projectsDbHelper";
 import {ProjectStatus} from "../models/ProjectStatus";
 
@@ -65,6 +65,8 @@ class ProjectsStore {
     this.projects = this.projects.filter(p => p.id !== projectId);
     this.selectedProject = null;
     deleteProject(projectId);
+    deleteCountersForProject(projectId);
+    deleteImagesForProject(projectId);
   };
 
   @action
