3fb363336e732a5e1594f8efa66b2adcdf467686
Rommel Rico <me@rommelrico.com>
2020-02-22 12:11:09 -0800

    :sparkles: StoreReview: Calculating if allowed to ask for review

--- store/AppSettingsStore.js
+++ store/AppSettingsStore.js
@@ -17,6 +17,25 @@ class AppSettingsStore {
   @computed get mainBGColor() { return this.settings ? this.settings.mainBGColor : Colors.primaryBGColor; }
   @computed get filterPreference() { return this.settings ? this.settings.filterPreference : FilterPreference.WIP; }
 
+  @computed get allowedToAskForReview() {
+    const {interactionsTowardsReviewAsk, lastAskedToReviewDate} = this.settings;
+
+    const MIN_INTERACTIONS = 50;
+    const MIN_DAYS_BETWEEN_ASK = 14;
+    const FOURTEEN_DAYS = MIN_DAYS_BETWEEN_ASK * 24 * 60 * 60 * 1000; // Not all days are 24 hours, but whatever.
+    const enoughInteractions = interactionsTowardsReviewAsk >= MIN_INTERACTIONS;
+
+    if (!lastAskedToReviewDate) {
+      // Never asked before so we can ask as long as they have more than MIN_INTERACTIONS;
+      return this.appOpened && enoughInteractions;
+    } else {
+      // Calculate days since last ask. Only ask at most every 2 weeks.
+      const today = +new Date();
+      const enoughDays = (today - FOURTEEN_DAYS) >= lastAskedToReviewDate;
+      return this.appOpened && enoughInteractions && enoughDays;
+    }
+  }
+
   // Actions
   @action setSettings = (settings) => this.settings = settings;
 
