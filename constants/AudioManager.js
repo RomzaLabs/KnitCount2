b83777cfb8da77897f10a5760a7e8eb170d1da4d
Rommel Rico <me@rommelrico.com>
2020-02-24 21:10:35 -0800

    :sparkles: Audio: Adding Rip sound handling

--- constants/AudioManager.js
+++ constants/AudioManager.js
@@ -3,6 +3,7 @@ import { Audio } from 'expo-av';
 import { Taps, Rips, Complete } from "./Sounds";
 
 const bubblePop = require("../assets/sounds/taps/bubble-pop.wav");
+const frog = require("../assets/sounds/rips/frog.wav");
 const yay = require("../assets/sounds/complete/yay.mp3");
 
 class AudioManager {
@@ -20,6 +21,20 @@ class AudioManager {
       console.error("Unable to play tap sound: ", error);
     }
   };
+
+  playRipSound = async(name) => {
+    const soundObject = new Audio.Sound();
+    try {
+      switch (name) {
+        case Rips.frog:
+        default:
+          await soundObject.loadAsync(frog);
+          await soundObject.playAsync();
+      }
+    } catch (error) {
+      console.error("Unable to play rip sound: ", error);
+    }
+  }
 
 }
 
