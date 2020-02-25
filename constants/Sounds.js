39e9d007be8fe7dd5aff4fe2f402c9529fadc174
Rommel Rico <me@rommelrico.com>
2020-02-24 21:03:50 -0800

    :sparkles: Audio: Adding yay sound to Sounds constant

--- constants/Sounds.js
+++ constants/Sounds.js
@@ -1,3 +1,9 @@
-export default {
-  bubblePop: "../../assets/sounds/bubble-pop.wav"
+export const Taps = {
+  bubblePop: "/assets/sounds/taps/bubble-pop.wav"
+};
+
+export const Rips = {};
+
+export const Complete = {
+  yay: "/assets/sounds/complete/yay.mp3"
 };
