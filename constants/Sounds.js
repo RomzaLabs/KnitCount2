c3fbc1870f4b15cd5ea2d64d6dbc3ca0cf37d20f
Rommel Rico <me@rommelrico.com>
2020-03-02 21:12:42 -0800

    :sparkles: SoundSettings: New audio paths in Sounds.js

--- constants/Sounds.js
+++ constants/Sounds.js
@@ -1,11 +1,20 @@
 export const Taps = {
-  default: "/assets/sounds/taps/default_tap.wav"
+  default: "/assets/sounds/taps/default_tap.wav",
+  brick: "/assets/sounds/taps/brick_tap.wav",
+  cat: "/assets/sounds/taps/cat_tap.wav",
+  lightswitch: "/assets/sounds/taps/lightswitch_tap.wav"
 };
 
 export const Rips = {
-  default: "/assets/sounds/rips/default_rip.wav"
+  default: "/assets/sounds/rips/default_rip.wav",
+  brick: "/assets/sounds/taps/brick_rip.wav",
+  cat: "/assets/sounds/taps/cat_rip.wav",
+  lightswitch: "/assets/sounds/taps/lightswitch_rip.wav"
 };
 
 export const Complete = {
-  default: "/assets/sounds/complete/default_complete.mp3"
+  default: "/assets/sounds/complete/default_complete.mp3",
+  brick: "/assets/sounds/taps/brick_complete.wav",
+  cat: "/assets/sounds/taps/cat_complete.wav",
+  lightswitch: "/assets/sounds/taps/lightswitch_complete.wav"
 };
