478b44ccf887dea268c3b94dd313075314d37df4
Rommel Rico <me@rommelrico.com>
2020-03-04 07:25:32 -0800

    :sparkles: SoundSettings: Changing Sounds to simple key-value

--- constants/Sounds.js
+++ constants/Sounds.js
@@ -1,20 +1,9 @@
-class AudioPack {
-  constructor(name) {
-    this.tap = `/assets/sounds/taps/${name}_tap.wav`;
-    this.rip = `/assets/sounds/rips/${name}_rip.wav`;
-    this.complete = `/assets/sounds/complete/${name}_complete.mp3`;
-  }
-}
-
-const defaultPack = new AudioPack("default");
-const brickPack = new AudioPack("brick");
-const catPack = new AudioPack("cat");
-const lightswitchPack = new AudioPack("lightswitch");
-
-export const Sounds = {
-  none: "no-sounds",
-  default: defaultPack,
-  brick: brickPack,
-  cat: catPack,
-  lightswitch: lightswitchPack
+const Sounds = {
+  none: "none",
+  default: "default",
+  brick: "brick",
+  cat: "cat",
+  lightswitch: "lightswitch"
 };
+
+export default Sounds;
