2b58fff9895b172508eae330d5fe2df3d939ee7c
Rommel Rico <me@rommelrico.com>
2020-03-07 11:50:44 -0800

    :sparkles: SoundSettings: Adding SOUND_DATA constant

--- constants/Sounds.js
+++ constants/Sounds.js
@@ -6,4 +6,12 @@ const Sounds = {
   lightswitch: "lightswitch"
 };
 
+export const SOUND_DATA = [
+  { id: '1', title: Sounds.none },
+  { id: '2', title: Sounds.default },
+  { id: '3', title: Sounds.brick },
+  { id: '4', title: Sounds.cat },
+  { id: '5', title: Sounds.lightswitch }
+];
+
 export default Sounds;
